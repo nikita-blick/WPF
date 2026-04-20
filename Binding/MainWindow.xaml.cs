6d6a27dbdd5c178a659f00cbdf332f8e1d10ffa8
Nikita <Nikita@DESKTOP-13IEN5T>
2026-04-20 22:22:52 +0500

    Add project files.

---
+++ Binding/MainWindow.xaml.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using System.Runtime.CompilerServices;
+using System.ComponentModel;
+
+
+namespace Binding
+{
+	/// <summary>
+	/// Interaction logic for MainWindow.xaml
+	/// </summary>
+	public partial class MainWindow : Window, INotifyPropertyChanged
+	{
+		public MainWindow()
+		{
+			DataContext = this;
+			InitializeComponent();
+		}
+		public event PropertyChangedEventHandler PropertyChanged;
+		string boundText;
+		public string BoundText
+		{
+			get => boundText;
+			set
+			{
+				boundText = value;
+				OnPropertyChanged();
+			}
+		}
+		void OnPropertyChanged([CallerMemberName] string propertyName = null)
+		{
+			PropertyChanged.Invoke(this, new PropertyChangedEventArgs(propertyName));
+			//PropertyChanged.Invoke(this, new PropertyChangedEventArgs(nameof(BoundText)));
+			//PropertyChanged.Invoke(this,new PropertyChangedEventArgs(boundText));
+		}
+
+		private void BtnSet_Click(object sender, RoutedEventArgs e)
+		{
+			BoundText = "Default";
+			txtInput.Focus();
+		}
+	}
+}
